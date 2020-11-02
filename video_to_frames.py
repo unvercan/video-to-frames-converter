e7bf2d06a94ec2e52166a753ed83b25d5a335d16
Ünver Can Ünlü <unverunlu@cfm.com.tr>
2020-11-02 09:36:26 +0300

    function is added.

--- video_to_frames.py
+++ video_to_frames.py
@@ -1,7 +1,6 @@
 # imports
 from argparse import ArgumentParser
 from os import path, makedirs
-
 import cv2 as cv
 
 
@@ -21,9 +20,9 @@ def main():
                                  help='path to output frames directory')
     argument_parser.add_argument('-p', '--prefix',
                                  type=str,
-                                 default='frame',
+                                 default='',
                                  help='frame prefix')
-    argument_parser.add_argument('-f', '--format',
+    argument_parser.add_argument('-f', '--image-format',
                                  type=str,
                                  default='jpg',
                                  help='frame as image format')
@@ -32,44 +31,49 @@ def main():
     arguments = argument_parser.parse_args()
 
     # parameters
-    frames_directory = arguments.output
+    input = arguments.input_base
+    output = arguments.output_base
     prefix = arguments.prefix
-    video = arguments.input
-    format = arguments.format
+    format = arguments.image_format
 
+    # convert video to frames
+    video_to_frames(input=input, output=output, prefix=prefix, format=format)
+
+
+# convert video to frames function
+def video_to_frames(input, output, prefix, format):
     # info
     print('Video to Frames Converting')
     print('*' * 50)
-    print('input: "{input}"'.format(input=video))
-    print('output: "{output}"'.format(output=frames_directory))
+    print('input: "{input}"'.format(input=input))
+    print('output: "{output}"'.format(output=output))
     print('prefix: "{prefix}"'.format(prefix=prefix))
     print('format: "{format}"'.format(format=format))
 
-    # check frame directory exists
-    if not path.exists(frames_directory):
-        print('"{output}" directory does not exist.'.format(output=frames_directory))
-        makedirs(frames_directory)
-        print('"{output}" directory is created.'.format(output=frames_directory))
+    # check output exists
+    if not path.exists(output):
+        print('"{output}" directory does not exist.'.format(output=output))
+        makedirs(output)
+        print('"{output}" directory is created.'.format(output=output))
 
     # capture video
     print('Converting is started.')
-    video_capture = cv.VideoCapture(video)
+    video_capture = cv.VideoCapture(input)
 
     # read frame
-    print('"{input}" is reading.'.format(input=video))
+    print('"{input}" is reading.'.format(input=input))
     success, frame = video_capture.read()
 
-    # loop
+    # loop for frames
     frame_count = 0
     while success:
         # save frame as image
-        frame_file_name = '{prefix}{frame_count}.{format}'.format(prefix=prefix,
-                                                                  frame_count=frame_count,
-                                                                  format=format)
-        frame_path = path.join(frames_directory, frame_file_name)
+        frame_file_name = '{prefix}{frame_count:05d}.{format}'.format(prefix=prefix, frame_count=frame_count,
+                                                                      format=format)
+        frame_path = path.join(output, frame_file_name)
         cv.imwrite(frame_path, frame)
         print('"{frame_file}" is saved into "{frames_directory}".'.format(frame_file=frame_file_name,
-                                                                          frames_directory=frames_directory))
+                                                                          frames_directory=output))
 
         # read next frame
         success, frame = video_capture.read()
